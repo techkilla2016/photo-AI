7da6906677db47b0a305cfaf18fe95e453ec710e
techkilla2016 <66670136+techkilla2016@users.noreply.github.com>
2024-02-22 11:14:30 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,7 +70,7 @@ const Home = () => {
 
 
   const handleGenrate = async () => {
-    const api = 'https://eecc-103-17-110-127.ngrok-free.app/rec';
+    const api = 'https://03f2-103-17-110-127.ngrok-free.app/rec';
     setIsLoad(true)
     try {
       const res = await axios.post(api, {
